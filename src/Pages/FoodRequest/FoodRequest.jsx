be1a6168268078a9370dc67a478132e13d195f88
Ashraf-2 <hasanashraful03@gmail.com>
2023-11-30 11:15:09 +0600

    added delete and show available food by food request cancle

--- src/Pages/FoodRequest/FoodRequest.jsx
+++ src/Pages/FoodRequest/FoodRequest.jsx
@@ -1,8 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Auth/AuthProvider";
 import { useQueries, useQuery } from "@tanstack/react-query";
 import UseRequestData from "../../Hooks/UseRequestData";
+import axios from "axios";
 
 const FoodRequest = () => {
     // const requestedFood = useLoaderData();
@@ -26,8 +27,13 @@ const FoodRequest = () => {
     const myFoodRequest = data && data?.filter(singleData => singleData.requester_email === user?.email)
     console.log("my food request: ", myFoodRequest);
 
+    // const [showFoods,setShowFoods] = useState(myFoodRequest);
+    // console.log(showFoods);
+
     if(isFetching || isLoading){
-        return <span className="loading loading-spinner loading-lg"></span>
+        return (<div className="flex justify-center items-center my-10">
+            <span className=" text-center loading loading-spinner loading-lg"></span>
+        </div>)
     }
 
     // const myFoodRequest = requestedFood.filter(food => food.requester_email === user?.email) 
@@ -36,6 +42,57 @@ const FoodRequest = () => {
 
     // const {_id,refId,food_img,food_name,food_status,donator_name,donator_email,donator_photo,food_quantity,pickup_location,expire_date,notes,delivery_status,donation_amount,request_date,requester_name,requester_email,requester_photo} = requested_food;
 
+    const handleCanclartion = (food,id) => {
+        console.log("cancel id: ", id);
+        const {
+            refId,
+            food_img,
+            food_name,
+            food_status,
+            donator_name,
+            donator_email,
+            donator_photo,
+            food_quantity,
+            pickup_location,
+            expire_date,
+            notes,
+        } = food;
+        console.log('food: ',food);
+        console.log('food refid: ',refId);
+        //two work. 1. modify the delivery status in available list that "available", 2. delete this food from the foodRequest collection.
+        
+
+        //Modify the delivery status in available collection.
+        const updateFood = {
+            food_img,
+            food_name,
+            food_status,
+            donator_name,
+            donator_email,
+            donator_photo,
+            food_quantity,
+            pickup_location,
+            expire_date,
+            notes,
+            delivery_status: "available"
+        }
+        console.log('update food: ', updateFood);
+        axios.put(`http://localhost:5000/availableFoods/${refId}`, updateFood)
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
+
+        //delete this food from the foodRequest collections
+        axios.delete(`http://localhost:5000/foodRequest/${id}`)
+        .then(res => console.log(res.data))
+        .catch(error => console.log(error))
+
+
+        // axios.get('http://localhost:5000/foodRequest')
+        // .then(res => {
+        //     setShowFoods(res.data)
+        // })
+        // .catch(error => console.log(error))
+    }
 
     return (
         <div>
@@ -87,13 +144,13 @@ const FoodRequest = () => {
                                     <p>{oneFood.request_date}</p>
                                 </td>
                                 <td >
-                                    <p>{oneFood.donation_amount}</p>
+                                    <p className="text-center">$ {oneFood.donation_amount}</p>
                                 </td>
                                 <td >
                                     <p className="badge badge-accent py-2 ">{oneFood.delivery_status.toUpperCase()}</p>
                                 </td>
                                 <td >
-                                    <button className="btn btn-info">Cancel Request</button>
+                                    <button onClick={() => handleCanclartion(oneFood,oneFood._id)} className="btn btn-info">Cancel Request</button>
                                 </td>
                             </tr>)
                         }
